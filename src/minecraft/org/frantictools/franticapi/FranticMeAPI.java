f749e00c918e70ba1fa8dd9b56d83d8d888e67e9
Ben Johnson <johnsonbben@gmail.com>
2012-07-27 00:10:08 -0700

    Added post dispatch function, easier code

--- src/minecraft/org/frantictools/franticapi/FranticMeAPI.java
+++ src/minecraft/org/frantictools/franticapi/FranticMeAPI.java
@@ -4,10 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.Map;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
@@ -43,15 +45,10 @@ public class FranticMeAPI
 				{
 					String target = "http://www.franticme.com/?r=login";
 
-					HttpPost httpPost = new HttpPost(target);
-
 					BasicNameValuePair[] params =
 					{ new BasicNameValuePair("franticPostUsername", username), new BasicNameValuePair("franticPostPassword", password), new BasicNameValuePair("api", "FMOD") };
-
-					UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(Arrays.asList(params));
-					urlEncodedFormEntity.setContentEncoding(HTTP.UTF_8);
-					httpPost.setEntity(urlEncodedFormEntity);
-					HttpResponse response = client.execute(httpPost);
+					
+					HttpResponse response = dispatchPostRequest(target, params, client);
 
 					String rawData = getResult(response);
 
@@ -110,5 +107,15 @@ public class FranticMeAPI
 			rawData = rawData + line + "\r\n";
 		}
 		return rawData;
+	}
+	
+	private static HttpResponse dispatchPostRequest(String target, BasicNameValuePair[] params, HttpClient client) throws ClientProtocolException, IOException
+	{
+		HttpPost httpPost = new HttpPost(target);
+
+		UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(Arrays.asList(params));
+		urlEncodedFormEntity.setContentEncoding(HTTP.UTF_8);
+		httpPost.setEntity(urlEncodedFormEntity);
+		return client.execute(httpPost);
 	}
 }
